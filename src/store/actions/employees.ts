110c07eb491313f22d4b4526213a68edbd73505e
jamesaspinall <james.aspinall@agridigital.io>
2021-01-08 17:30:24 +1100

    Addd meta tags

--- src/store/actions/employees.ts
+++ src/store/actions/employees.ts
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 interface IEmployee {
   firstName: string;
   lastName: string;
@@ -7,6 +8,13 @@ interface IEmployee {
 
 export const fectchAllEmployees = () => (dispatch: any, getState: any) => {
   console.log("I RAN!");
+  const transaction = Sentry.startTransaction({
+    name: "test-transaction-JAMES",
+  });
+  const span = transaction.startChild({
+    op: "fectchAllEmployees",
+    description: `GET`,
+  }); // This function returns a Span
   const runAsync = async () => {
     const apiResponse = await fetch("http://34.220.1.118:3000/", {
       method: "GET",
@@ -14,7 +22,10 @@ export const fectchAllEmployees = () => (dispatch: any, getState: any) => {
         Accept: "*/*",
       },
     });
+    span.setTag("Response Status Code:", apiResponse.status);
     const tableData = await apiResponse.json();
+    span.setData("AllEmployeeData", tableData);
+    span.finish();
     console.log(tableData);
     dispatch({
       type: "SET_ALL_EMPLOYEES_TABLE_DATA",
